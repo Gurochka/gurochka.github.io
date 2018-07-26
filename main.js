57b46cc515ae02c1f89bd0f3ecd46346d04a34c2
gurochka <paradise-in-hell@yandex.ru>
2018-07-26 14:25:27 +0400

    new file

---
+++ main.js
@@ -0,0 +1,9 @@
+var Main = { 
+	template: `
+	    <div class="card">
+	      <div class="card-body">
+	        <p>Тыж программист, придется немного потрудиться.</p>
+	      </div>
+	    </div>
+	`
+}
